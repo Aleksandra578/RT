d46f54f1c682eecf6cdb5a2412e5a86265a762af
Александра <aleksandravoron-01@yandex.ru>
2024-12-23 17:34:14 +0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    chrome_driver = "C:\skill\edgedriver\msedgedriver.exe"
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'aleksandravoron-01@yandex.ru'
+    valid_password = '22Aleks2024!'
+    valid_email_for_reg = 'aleksandravoron-01@yandex.ru'
+    invalid_email = 'RRTT@gmail.com'
+    invalid_password = 'QAZPLMqwee555'
+    empty_email = ''
+    empty_password = ''
+    menu_of_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name_of_user = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    f_name = 'Александра'
+    l_name = 'Воронцова'
+    f_name_ = '-'
+    l_name_ = '-'
+    region_ = 'Удмуртская Республика'
+    russian_generate_string = 'У'
+    latin_generate_string = 'llkjdvbn'
+    chinese_chars = '会汉么样会'
+    special_chars = '@+---##$$%^'
+    numbers = 1234567890
+    empty = ''
+    passw1 = 'Kl25479G'
+    passw2 = 'Kvbsdl5752kjhxd'
+    passw3 = 'HGF56166KLJHGHFGDFn'
